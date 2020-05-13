2411240439719c92dea96261816082655ae6aeac
Parth Patel <parthpatel1105@gmail.com>
2020-05-13 13:54:27 -0400

    Test

---
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -0,0 +1,25 @@
+//
+//  MinutesTests.swift
+//  MinutesTests
+//
+//  Created by Parth Patel on 13/05/20.
+//  Copyright © 2020 Microsoft. All rights reserved.
+//
+
+import XCTest
+@testable import Minutes
+
+class MinutesTests: XCTestCase {
+
+    var entryUnderTest: Entry!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+}
